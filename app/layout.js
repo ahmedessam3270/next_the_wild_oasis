2494424039d556cab7c9a05fd5f39e00cdeaebae
ahmed <ahmedessam201790@gmail.com>
2024-06-25 07:53:23 +0300

    adding the bare bones pages

--- app/layout.js
+++ app/layout.js
@@ -26,8 +26,8 @@ export default function RootLayout({ children }) {
         className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}
       >
         <Header />
-        <div className="flex-1 px-4 py-6 md:px-8 md:py-12 ">
-          <main className="max-w-6xl mx-auto">{children}</main>
+        <div className="flex-1 px-4 py-6 md:px-8 md:py-12 grid">
+          <main className="max-w-7xl mx-auto w-full">{children}</main>
         </div>
       </body>
     </html>
