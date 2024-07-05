3579139f1ce80a4c77c47f32da8a66837f6fb722
ahmed <ahmedessam201790@gmail.com>
2024-07-05 14:17:35 +0300

    fixing ReservationReminder layout to be responsive in the mobile view

--- app/_components/ReservationReminder.js
+++ app/_components/ReservationReminder.js
@@ -9,7 +9,7 @@ function ReservationReminder() {
   if (!range.from || !range.to) return null;
 
   return (
-    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-5 px-8 rounded-full bg-accent-500 text-primary-800 text  font-semibold shadow-xl shadow-slate-900 flex gap-8 items-center">
+    <div className="fixed bottom-6 left-1/2 -translate-x-1/2 py-2.5 px-3 md:py-5 md:px-8 rounded-full bg-accent-500 text-primary-800 font-semibold shadow-xl shadow-slate-900 flex lg:text-lg items-center justify-between w-10/12 md:w-6/12 lg:w-1/3 ">
       <p>
         <span>👋</span> Don&apos;t forget to reserve your dates <br /> from{" "}
         {format(new Date(range.from), "MMM dd yyyy")} to{" "}
