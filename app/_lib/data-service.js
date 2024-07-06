17c639acc589ceba7a2b8b7e6a5d8f9055b6f4f4
ahmed <ahmedessam201790@gmail.com>
2024-07-06 19:30:41 +0300

    updating user profile with actions and revalidating the inputs using revalidatePath and also introducing useFormStatus to perform a loading effect to show the user that their input is being submitting

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -182,15 +182,18 @@ export async function createBooking(newBooking) {
 /////////////
 // UPDATE
 
+/*
+
+
 // The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
   const { data, error } = await supabase
-    .from("guests")
-    .update(updatedFields)
-    .eq("id", id)
-    .select()
-    .single();
-
+  .from("guests")
+  .update(updatedFields)
+  .eq("id", id)
+  .select()
+  .single();
+  
   if (error) {
     console.error(error);
     throw new Error("Guest could not be updated");
@@ -200,12 +203,12 @@ export async function updateGuest(id, updatedFields) {
 
 export async function updateBooking(id, updatedFields) {
   const { data, error } = await supabase
-    .from("bookings")
-    .update(updatedFields)
-    .eq("id", id)
-    .select()
-    .single();
-
+  .from("bookings")
+  .update(updatedFields)
+  .eq("id", id)
+  .select()
+  .single();
+  
   if (error) {
     console.error(error);
     throw new Error("Booking could not be updated");
@@ -218,10 +221,12 @@ export async function updateBooking(id, updatedFields) {
 
 export async function deleteBooking(id) {
   const { data, error } = await supabase.from("bookings").delete().eq("id", id);
-
+  
   if (error) {
     console.error(error);
     throw new Error("Booking could not be deleted");
   }
   return data;
 }
+
+*/
