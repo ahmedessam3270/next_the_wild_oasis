5d2929cfdbe9e79993d7ec781c71aa502e9fdd2e
ahmed <ahmedessam201790@gmail.com>
2024-07-08 15:20:26 +0300

    my implementation for the challenge of creating and implementing updateReservation Functionality

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -20,6 +21,36 @@ export async function updateGuest(formData) {
 
   if (error) throw new Error("Guest could not be updated");
   revalidatePath("/account/profile");
+}
+
+export async function updateReservation(updatedFields) {
+  const session = await auth();
+  const reservationId = +updatedFields.get("id");
+  if (!session) throw new Error("You must be logged in");
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(reservationId)) {
+    throw new Error("You are not allowed to delete this booking");
+  }
+
+  const id = +updatedFields.get("id");
+  const numGuests = +updatedFields.get("numGuests");
+  const observations = updatedFields.get("observations");
+  const latestUpdatedFields = { id, numGuests, observations };
+
+  const { data, error } = await supabase
+    .from("bookings")
+    .update(latestUpdatedFields)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+  revalidatePath(`/account/reservations/edit/${id}`);
+  redirect("/account/reservations");
 }
 
 export async function deleteReservation(bookingId) {
